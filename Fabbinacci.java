d89f4e20d59b206c55667c5d5ca488d73fcd6c5d
Kartavya0206 <112715811+Kartavya0206@users.noreply.github.com>
2023-09-01 18:27:33 +0530

    Add files via upload

---
+++ Fabbinacci.java
@@ -0,0 +1,21 @@
+import java.util.* ;
+public class Fabbinacci {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int a=0;
+        System.out.print(0+" ");
+        int b=1;
+        System.out.print(1+" ");
+        for(int count=2 ; count<=n ; count ++)
+        {
+            int temp =b;
+            b=b+a;
+            System.out.print(b+ " ");
+            a=temp;
+        }
+        sc.close();
+        }
+    }
+
